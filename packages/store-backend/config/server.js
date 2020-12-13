a4ff176eb6e5743b726b6e92c867114c142e1935
karthikbalajikb <karthik.akb009@gmail.com>
2020-12-13 15:36:58 +0530

    initial commit for store-admin

---
+++ packages/store-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '657c858c419151784e5d50929759aaa3'),
+    },
+  },
+});
---
+++ packages/store-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4a5d8901-2062-40c9-9374-a408a8eb81f5'
+};
